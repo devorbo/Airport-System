7897cb4df2c0adecc8721ca57d21c69d7a935d5a
DB <45538610+devorbo@users.noreply.github.com>
2022-08-03 17:22:02 +0300

    Update Flight.java

--- Flight.java
+++ Flight.java
@@ -6,17 +6,17 @@ public class Flight extends Thread {
 	private Airport airLand;
 	
 	public Flight(int numFlight,Airport airDepart,Airport airLand) {
-		this.numFlight=numFlight;
-		this.airDepart=airDepart;
-		this.airLand=airLand;
+		this.numFlight = numFlight;
+		this.airDepart = airDepart;
+		this.airLand = airLand;
 	}
 	
 	@Override
 	public void run() {
 		//flight simulation
 		System.out.println(airDepart.getName()+" to "+airLand.getName()+" Flight "+numFlight);
-		Random rnd=new Random();
-		int rndSec=(rnd.nextInt(4)+2)*100;
+		Random rnd = new Random();
+		int rndSec = (rnd.nextInt(4)+2)*100;
 		int routeDepart=this.airDepart.depart(numFlight);
 		//depart time
 		try {
@@ -26,7 +26,7 @@ public class Flight extends Thread {
 		}
 		this.airDepart.freeRunway(numFlight,routeDepart);//free route
 		//flight time
-		rndSec=(rnd.nextInt(4)+2)*100;//between 200 and 500 milliseconds
+		rndSec = (rnd.nextInt(4)+2)*100;//between 200 and 500 milliseconds
 		try {
 			sleep(rndSec);
 		} catch (InterruptedException e) {
